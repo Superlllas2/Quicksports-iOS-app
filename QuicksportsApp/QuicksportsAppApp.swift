9e504bc6450b6c90581f4785e481a37ecaf4091d
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2025-06-07 15:06:05 +0200

    Initial Commit

---
+++ QuicksportsApp/QuicksportsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuicksportsAppApp.swift
+//  QuicksportsApp
+//
+//  Created by Илья Невров on 07/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuicksportsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
